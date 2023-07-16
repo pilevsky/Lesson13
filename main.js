9fd09081bf26191594d20411ed7296f1571173d1
pilevsky <pilevskiyd@yandex.ru>
2023-07-16 16:25:13 +0600

    Lesson13

---
+++ main.js
@@ -0,0 +1,51 @@
+// document.querySelector(".one").onclick = function (event) {
+//   console.log(event);
+//   console.log("block 1");
+// };
+// document.querySelector(".two").oncontextmenu = function () {
+//   console.log("block 2");
+//   return false;
+// };
+// let width = 100;
+// document.querySelector(".there").onmousemove = function () {
+//   console.log("Это блок 3");
+//   document.querySelector('.there').style.width = width + 'px';
+//   width++;
+// };
+// document.querySelector(".there").onmouseenter = function () {
+//   document.querySelector(".there").style.background = "pink";
+// };
+// document.querySelector(".there").onmouseleave = function () {
+//   document.querySelector(".there").style.background = "white";
+// };
+// document.querySelector(".there").onmousedown = function () {
+//   document.querySelector(".there").style.background = "green";
+// };
+// document.querySelector(".there").onmouseup = function () {
+// document.querySelector(".there").style.background = "yellow";
+// };
+
+// document.querySelector(".inp").oninput = function (event) {
+//  console.log(event);
+// };
+// document.querySelector(".inp").onkeypress = function (event) {
+//  console.log(event.charCode);
+//  console.log(event.code);
+//   console.log(event);
+//   console.log(event.keyCode);
+//   console.log(event.key);
+// };
+// document.querySelector(".inp").onkeydown = function (event) {
+//   console.log(event.charCode);
+//   console.log(event.code);
+//   console.log(event);
+//   console.log(event.keyCode);
+//   console.log(event.key);
+// };
+// document.querySelector(".inp").onkeyup = function (event) {
+//   console.log(event.charCode);
+//   console.log(event.code);
+//   console.log(event);
+//   console.log(event.keyCode);
+//   console.log(event.key);
+// }
